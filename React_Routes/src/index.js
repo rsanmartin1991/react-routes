3a3789b20cd33b08118859f3bb1d176043f0bda1
Roberto San Martin <rjsm_1991@hotmail.com>
2018-12-06 13:39:02 -0500

    first commit

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -6,11 +6,31 @@ import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
-// your code goes here
 class Home extends Component{
   render(){
     return(
-      // your code goes here
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Happy">Happy</Link>
+            </li>
+            <li>
+              <Link to="/Guilty">Guilty</Link>
+            </li>
+            <li>
+              <Link to="/Sleepy">Sleepy</Link>
+            </li>
+          </ul>
+          <Route path="/Home"  component={<nav></nav>} />
+          <Route path="/Happy" component={Happy}/>
+          <Route path="/Guilty" component={Guilty}/>
+          <Route path="/Sleepy" component={Sleepy}/>
+        </div>
+      </Router>
     )
   }
 }
